65fba5891efce51e5e19749b4e9be6d2309ffe05
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-09-26 12:56:12 +1000

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  return (
+  <nav>
+    <a href="#home">{links[0]}</a>
+    <a href="#about">{links[1]}</a>
+    <a href="#projects">{links[2]}</a>
+  </nav>);
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
 }
 
 export default NavBar;
